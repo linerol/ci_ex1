729ac784c119c55490f5dfd25e705bff1daaf8c9
linerol <sessi.tchecounnou@epitech.eu>
2025-04-14 14:42:33 +0200

    feat: initialize project with TypeScript, ESLint, and Prettier
    - Added package.json with scripts for development, linting, and formatting.
    - Created basic HTTP server in app.ts serving HTML and CSS.
    - Implemented styles in styles.css with CSS variables and button styles.
    - Configured TypeScript with tsconfig.json for strict type checking and output directory.
    - Set up SASS for styling with a watch command.

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    const html = `
+      <html>
+        <head>
+          <title>My First Node.js App</title>
+          <link rel="stylesheet" href="/public/css/styles.css"/>
+        </head>
+        <div class="app__message">
+          <h1>Hello World</h1>
+        </div>
+      </html>
+    `;
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  } else if (req.url === "/public/css/styles.css") {
+    fs.readFile("./public/css/styles.css", (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("File Not Found");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(data);
+      }
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
